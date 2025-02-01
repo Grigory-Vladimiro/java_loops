a33538fb41b601ef5dd13ff30896ecb9692dd65b
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-01 20:48:40 +0100

    test1_try_ok=>update_of_main_java_file

--- java_loops/src/test/java/dev/grigory/java_loops/JavaLoopsClassTest.java
+++ java_loops/src/test/java/dev/grigory/java_loops/JavaLoopsClassTest.java
@@ -7,7 +7,7 @@ class JavaLoopsClassTest {
     @Test
     void testMultiplicationTableGeneration() {
         JavaLoopsClass loops = new JavaLoopsClass();
-        int[][] expected = new int [] [] {
+        int[][] expected = {
             {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
             {2, 4, 6, 8, 10, 12, 14, 16, 18, 20},
             {3, 6, 9, 12, 15, 18, 21, 24, 27, 30},
@@ -17,10 +17,14 @@ class JavaLoopsClassTest {
             {7, 14, 21, 28, 35, 42, 49, 56, 63, 70},
             {8, 16, 24, 32, 40, 48, 56, 64, 72, 80},
             {9, 18, 27, 36, 45, 54, 63, 72, 81, 90},
-            {10, 20, 30, 40, 50, 60, 70, 80, 90, 100},
-        assertArrayEquals(expected, loops.generateMultiplicationTable(10));
+            {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+        };
+        int[][] actual = loops.generateMultiplicationTable(10);
+        for (int i = 0; i < expected.length; i++) {
+            assertArrayEquals(expected[i], actual[i], "Row " + i + " is wrong");
+        }
         }
     }
-}
+
         
 
