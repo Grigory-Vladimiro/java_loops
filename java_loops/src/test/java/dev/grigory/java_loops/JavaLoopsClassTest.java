f9bd454c53b771a87dc5797a6be7a9f748c89d8c
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-01 21:26:02 +0100

    test_works_but_only_38%

--- java_loops/src/test/java/dev/grigory/java_loops/JavaLoopsClassTest.java
+++ java_loops/src/test/java/dev/grigory/java_loops/JavaLoopsClassTest.java
@@ -7,24 +7,15 @@ class JavaLoopsClassTest {
     @Test
     void testMultiplicationTableGeneration() {
         JavaLoopsClass loops = new JavaLoopsClass();
-        int[][] expected = {
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
-            {2, 4, 6, 8, 10, 12, 14, 16, 18, 20},
-            {3, 6, 9, 12, 15, 18, 21, 24, 27, 30},
-            {4, 8, 12, 16, 20, 24, 28, 32, 36, 40},
-            {5, 10, 15, 20, 25, 30, 35, 40, 45, 50},
-            {6, 12, 18, 24, 30, 36, 42, 48, 54, 60},
-            {7, 14, 21, 28, 35, 42, 49, 56, 63, 70},
-            {8, 16, 24, 32, 40, 48, 56, 64, 72, 80},
-            {9, 18, 27, 36, 45, 54, 63, 72, 81, 90},
-            {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
-        };
-        int[][] actual = loops.generateMultiplicationTable(10);
-        for (int i = 0; i < expected.length; i++) {
-            assertArrayEquals(expected[i], actual[i], "Row " + i + " is wrong");
-        }
-        }
+        int number = 5;
+        int [] expected = new int[10];
+for (int i = 0; i < 10; i++) {
+    expected[i] = number * (i + 1);
+}
+        assertArrayEquals(expected, loops.generateMultiplicationTable(number));
     }
+}
+    
 
         
 
