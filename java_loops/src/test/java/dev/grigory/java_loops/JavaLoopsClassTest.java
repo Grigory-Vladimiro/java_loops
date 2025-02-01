42b6b1f3e93c3cfd8e90e876f93efdfcaeafec4e
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-01 22:50:04 +0100

    redundant_test_deletion

--- java_loops/src/test/java/dev/grigory/java_loops/JavaLoopsClassTest.java
+++ java_loops/src/test/java/dev/grigory/java_loops/JavaLoopsClassTest.java
@@ -4,30 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaLoopsClassTest {
-    @Test
-    void testMultiplicationTableGeneration() {
-        JavaLoopsClass loops = new JavaLoopsClass();
-
-        
-        int number = 5;
-        int[] expected = new int[10];
-        for (int i = 0; i < 10; i++) {
-            expected[i] = number * (i + 1);
-        }
-        assertArrayEquals(expected, loops.generateMultiplicationTable(number));
-
-      
-        int[] expectedZero = new int[10];
-        assertArrayEquals(expectedZero, loops.generateMultiplicationTable(0));
-
-        
-        int[] expectedNegative = new int[10];
-        for (int i = 0; i < 10; i++) {
-            expectedNegative[i] = -3 * (i + 1);
-        }
-        assertArrayEquals(expectedNegative, loops.generateMultiplicationTable(-3));
-    }
-    @Test
+@Test
     void testMultiplicationTableLength() {
         JavaLoopsClass loops = new JavaLoopsClass();
         int[] table = loops.generateMultiplicationTable(7);
@@ -48,5 +25,3 @@ void testMultiplicationTableFullCheck() {
     }
 }
 }
-        
-
