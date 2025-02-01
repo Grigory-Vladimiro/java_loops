362a0def9b756969a3d53896cde2d964c40659b8
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-01 22:23:53 +0100

    test_structure

--- java_loops/src/test/java/dev/grigory/java_loops/JavaLoopsClassTest.java
+++ java_loops/src/test/java/dev/grigory/java_loops/JavaLoopsClassTest.java
@@ -7,12 +7,25 @@ class JavaLoopsClassTest {
     @Test
     void testMultiplicationTableGeneration() {
         JavaLoopsClass loops = new JavaLoopsClass();
+
+        
         int number = 5;
-        int [] expected = new int[10];
-for (int i = 0; i < 10; i++) {
-    expected[i] = number * (i + 1);
-}
+        int[] expected = new int[10];
+        for (int i = 0; i < 10; i++) {
+            expected[i] = number * (i + 1);
+        }
         assertArrayEquals(expected, loops.generateMultiplicationTable(number));
+
+      
+        int[] expectedZero = new int[10];
+        assertArrayEquals(expectedZero, loops.generateMultiplicationTable(0));
+
+        
+        int[] expectedNegative = new int[10];
+        for (int i = 0; i < 10; i++) {
+            expectedNegative[i] = -3 * (i + 1);
+        }
+        assertArrayEquals(expectedNegative, loops.generateMultiplicationTable(-3));
     }
     @Test
     void testMultiplicationTableLength() {
