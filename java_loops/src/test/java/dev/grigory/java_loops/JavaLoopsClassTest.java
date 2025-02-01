f60cca16d72248b03ba00918801547fb1c3d7720
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-01 21:47:42 +0100

    current_version_of_3_tests

--- java_loops/src/test/java/dev/grigory/java_loops/JavaLoopsClassTest.java
+++ java_loops/src/test/java/dev/grigory/java_loops/JavaLoopsClassTest.java
@@ -20,7 +20,20 @@ for (int i = 0; i < 10; i++) {
         int[] table = loops.generateMultiplicationTable(7);
         assertEquals(10, table.length, "The table should contain 10 lines.");
 }
-}
+@Test
+void testMultiplicationTableFullCheck() {
+    JavaLoopsClass loops = new JavaLoopsClass();
+    
+    for (int num = 1; num <= 10; num++) {
+        int[] expected = new int[10];
 
+        for (int i = 0; i < 10; i++) {
+            expected[i] = num * (i + 1);
+        }
+
+        assertArrayEquals(expected, loops.generateMultiplicationTable(num));
+    }
+}
+}
         
 
